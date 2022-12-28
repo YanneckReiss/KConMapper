cf8a1dcaceabbb6a7fe5b276cde63ad5c7d1622f
Ynnck123 <yanneck.reiss@hotmail.de>
2022-12-28 20:16:50 +0100

    Updated publishing configuration

--- kconmapper-plugin/build.gradle.kts
+++ kconmapper-plugin/build.gradle.kts
@@ -16,7 +16,29 @@ publishing {
 
     publications {
         create<MavenPublication>("gpr") {
+            groupId = "com.github.yanneckreiss"
+            artifactId = "kconmapper"
+            version = "1.0.0-alpha01"
+
             from(components["java"])
+
+            pom {
+                name.set("KConMapper")
+                description.set("A KSP plugin for generating constructor mapping extension functions.")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("yanneckreiss")
+                        name.set("Yanneck Reiß")
+                        email.set("support@kaspic.de")
+                    }
+                }
+            }
         }
     }
 }
