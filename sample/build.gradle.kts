e8ab2e4e4da232ccf4d78d2287999c4067c97953
Yanneck Reiß <yanneck.reiss@gmail.com>
2023-01-25 22:00:48 +0100

    Updated docs for Android sourceSets  - Also removed unnecessary code from samples

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,19 +4,8 @@ plugins {
     idea
 }
 
-idea {
-    module {
-        // Not using += due to https://github.com/gradle/gradle/issues/8749
-        sourceDirs = sourceDirs + file("build/generated/ksp/main/kotlin") // or tasks["kspKotlin"].destination
-        testSourceDirs = testSourceDirs + file("build/generated/ksp/test/kotlin")
-        generatedSourceDirs = generatedSourceDirs + file("build/generated/ksp/main/kotlin") + file("build/generated/ksp/test/kotlin")
-    }
-}
-
-kotlin.sourceSets.main {
-    kotlin.srcDirs(
-        file("$buildDir/generated/ksp/main/kotlin"),
-    )
+sourceSets.configureEach {
+    kotlin.srcDir("$buildDir/generated/ksp/$name/kotlin/")
 }
 
 dependencies {
